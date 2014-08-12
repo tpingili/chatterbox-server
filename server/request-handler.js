9ec0d20d9f2d7f4861bdbfd46c572656a371041d
tpingili <ptejaswini.reddy@gmail.com>
2014-08-11 20:48:49 -0700

    Refactored code. Pulled Post,Get,Options, out of handler into own functions.

--- server/request-handler.js
+++ server/request-handler.js
@@ -21,12 +21,23 @@ exports.handleRequest = function(request, response) {
   /* Documentation for both request and response can be found at
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
   if(request.method === "GET"){
-    console.log("get request");
-    statusCode = 200;
-    response.writeHead(statusCode,headers);
-    response.end(JSON.stringify(messages));
+    return handleGet(request,response);
   }else if(request.method === "POST"){
-    console.log("post request");
+    return handlePost(request, response);
+  }else if(request.method === "OPTIONS"){
+    return handleOptions(request, response);
+  }
+};
+var handleOptions = function(request, response){
+    var statusCode = 200;
+    var headers = defaultCorsHeaders;
+    response.writeHead(statusCode,headers);
+    response.end();
+    console.log("Serving request type " + request.method + " for url " + request.url);
+};
+var handlePost = function(request, response){
+    var statusCode = 201;
+    var headers = defaultCorsHeaders;
     var data = "";
     var jsonData;
     request.on("data", function(bit){
@@ -35,35 +46,18 @@ exports.handleRequest = function(request, response) {
     request.on("end", function(){
       jsonData = JSON.parse(data);
       messages.results.push(jsonData);
+      response.writeHead(statusCode, headers);
+      console.log("Serving request type " + request.method + " for url " + request.url);
+      response.end("Posted");
     });
-    statusCode = 201;
-    response.writeHead(statusCode, headers);
-    response.end("Posted");
-  }else if(request.method === "OPTIONS"){
-    statusCode = 200;
-    response.writeHead(statusCode,headers);
-    response.end();
-    console.log("options request");
-  }
-  console.log("Serving request type " + request.method + " for url " + request.url);
-
-
-
-  /* Without this line, this server wouldn't work. See the note
-   * below about CORS. */
-
-
-
-  /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
-
-  /* Make sure to always call response.end() - Node will not send
-   * anything back to the client until you do. The string you pass to
-   * response.end() will be the body of the response - i.e. what shows
-   * up in the browser.*/
-  response.end("Hello, World!" );
 };
-
+var handleGet = function(request, response){
+  var statusCode = 200;
+  var headers = defaultCorsHeaders;
+  response.writeHead(statusCode,headers);
+  console.log("Serving request type " + request.method + " for url " + request.url);
+  response.end(JSON.stringify(messages));
+};
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
  * This CRUCIAL code allows this server to talk to websites that
  * are on different domains. (Your chat client is running from a url
