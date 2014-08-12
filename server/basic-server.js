bb4c3821b6eb181d9123943aadd5fc1b4296af92
tpingili <ptejaswini.reddy@gmail.com>
2014-08-11 20:18:35 -0700

    Passing first three requirements. Passing first set of Mocha Tests.

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,7 +1,6 @@
 /* Import node's http module: */
 var http = require("http");
-
-
+var requestHandler = require("./request-handler.js");
 /* Every server needs to listen on a port with a unique number. The
  * standard port for HTTP servers is port 80, but that port is
  * normally already claimed by another server and/or not accessible
@@ -13,13 +12,11 @@ var port = 3000;
  * special address that always refers to localhost. */
 var ip = "127.0.0.1";
 
-
-
 /* We use node's http module to create a server. Note, we called it 'server', but
 we could have called it anything (myServer, blahblah, etc.). The function we pass it (handleRequest)
 will, unsurprisingly, handle all incoming requests. (ps: 'handleRequest' is in the 'request-handler' file).
 Lastly, we tell the server we made to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
+var server = http.createServer(requestHandler.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
--- server/request-handler.js
+++ server/request-handler.js
@@ -4,23 +4,55 @@
  * You'll have to figure out a way to export this function from
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
-
-var handleRequest = function(request, response) {
+var messages = {results: []};
+exports.handleRequest = function(request, response) {
+  var statusCode;
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = "text/plain";
+  if (request.url !== '/classes/messages'){
+    statusCode = 404;
+    response.writeHead(statusCode,headers);
+    response.end();
+    return;
+  }
   /* the 'request' argument comes from nodes http module. It includes info about the
   request - such as what URL the browser is requesting. */
 
   /* Documentation for both request and response can be found at
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
-
+  if(request.method === "GET"){
+    console.log("get request");
+    statusCode = 200;
+    response.writeHead(statusCode,headers);
+    response.end(JSON.stringify(messages));
+  }else if(request.method === "POST"){
+    console.log("post request");
+    var data = "";
+    var jsonData;
+    request.on("data", function(bit){
+      data+=bit;
+    });
+    request.on("end", function(){
+      jsonData = JSON.parse(data);
+      messages.results.push(jsonData);
+    });
+    statusCode = 201;
+    response.writeHead(statusCode, headers);
+    response.end("Posted");
+  }else if(request.method === "OPTIONS"){
+    statusCode = 200;
+    response.writeHead(statusCode,headers);
+    response.end();
+    console.log("options request");
+  }
   console.log("Serving request type " + request.method + " for url " + request.url);
 
-  var statusCode = 200;
+
 
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
-  var headers = defaultCorsHeaders;
 
-  headers['Content-Type'] = "text/plain";
+
 
   /* .writeHead() tells our server what HTTP status code to send back */
   response.writeHead(statusCode, headers);
@@ -29,7 +61,7 @@ var handleRequest = function(request, response) {
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
+  response.end("Hello, World!" );
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -40,6 +72,6 @@ var handleRequest = function(request, response) {
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
+  "access-control-allow-headers": "content-type, accept, type",
   "access-control-max-age": 10 // Seconds.
 };
